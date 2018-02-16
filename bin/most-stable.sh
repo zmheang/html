fa1ab1cec58bb4bf97335188922d1242050fc7eb
harttle <harttle@harttle.com>
2018-02-17 01:08:45 +0800

    translate: cross-document-messaging

--- bin/most-stable.sh
+++ bin/most-stable.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-[ "$1" == "-t" ] || [ "$1" == "--table" ]
-table=$?
-
-if [ $table == 0 ]; then
-  echo '修改日期 | 文件 | 相对时间'
-  echo '---      | ---  | ---'
-fi
+echo '修改日期 | 文件 | 相对时间'
+echo '---      | ---  | ---'
 
 for file in $(find src -name *.en.html -type f)
 do
@@ -14,9 +9,5 @@ do
     continue
   fi
   dates=`git log -1 --format="%ai|%ar" -- $file`
-  if [ $table == 0 ]; then
-    echo ${dates%%|*} \| $file \| ${dates##*|}
-  else
-    echo  $file, updated ${dates##*|}
-  fi
+  echo ${dates%%|*} \| $file \| ${dates##*|}
 done | sort -n | head -n 20
