7c615a43da2db2f22aa653b82ba4638a4d7e30c1
harttle <harttle@harttle.com>
2018-02-26 15:51:11 +0800

    feature: translate infomation in final HTML outputs

--- bin/merge.js
+++ bin/merge.js
@@ -1,22 +1,63 @@
 const fs = require('fs');
+const spawnSync = require('child_process').spawnSync
 const path = require('path');
 const skeletonPath = path.resolve(__dirname, '../skeleton.html');
 
-var skeleton = fs.readFileSync(skeletonPath, 'utf8');
-var target = '',
-    begin = 0;
+const skeleton = fs.readFileSync(skeletonPath, 'utf8');
+const rFile = /<file src="([^"]*?)">/g;
+const root = path.resolve(__dirname, '..');
 
-var rFile = /<file src="([^"]*?)">/g,
-    match;
+let match;
+let target = '';
+let begin = 0;
+let fast = false;
+
+process.argv.forEach(arg => {
+  if (arg === 'fast' ) {
+    fast = true;
+    console.error('fast mode enabled, skipping file update date');
+  }
+});
 
 while (match = rFile.exec(skeleton)) {
-    var file = path.resolve(__dirname, '../src', match[1]);
-    var content;
+    let enPath = 'src/' + match[1] + '.en.html';
+    let zhPath = 'src/' + match[1] + '.zh.html';
+
+    let enModified = modified(enPath);
+    let zhModified = modified(zhPath);
+    let content;
     try {
-        content = fs.readFileSync(file + '.zh.html', 'utf8');
+      content = fs.readFileSync(path.resolve(root, zhPath), 'utf8');
     } catch (e) {
-        content = fs.readFileSync(file + '.en.html', 'utf8');
+        if (e.code === 'ENOENT') {
+          content = fs.readFileSync(path.resolve(root, enPath), 'utf8');
+        } else {
+          throw e;
+        }
     }
+
+    let zhLink = `https://github.com/whatwg-cn/html/edit/master/${zhPath}`;
+    let enLink = `https://github.com/whatwg-cn/html/blob/master/${enPath}`;
+
+    if (!zhModified) {
+      let content = '从 ' + enLink + ' 拷贝英文源码至此，开始翻译';
+      zhLink = `https://github.com/whatwg-cn/html/new/master/${zhPath}`;
+      zhLink += '?filename=' + path.basename(zhPath);
+      zhLink += '&value=' + encodeURIComponent(content);
+    }
+
+    content = content.replace(
+      /<h[2-6](\s[^>]+)?>[\s\S]*?<\/h[2-6]>/,
+      (match) => {
+        let attr =  'class="translate-info"';
+        attr += ` data-zh-file="${zhLink}"`;
+        attr += ` data-zh-date="${zhModified}"`;
+        attr += ` data-en-file="${enLink}"`;
+        attr += ` data-en-date="${enModified}"`;
+        return `${match}\n<div ${attr}></div>`;
+      }
+    )
+
     target += skeleton.slice(begin, match.index);
     target += content;
     begin = match.index + match[0].length;
@@ -24,3 +65,13 @@ while (match = rFile.exec(skeleton)) {
 
 target += skeleton.slice(begin);
 process.stdout.write(target);
+
+function modified(file) {
+  if (fast) return 'Unkown due to fast build';
+  let result = spawnSync('git', ['log', '-1', '--format="%ai (%ar)"', '--', file]);
+  if (result.error) {
+    throw result.error;
+  }
+  let str = String(result.stdout);
+  return str.trim().replace(/"/g, '');
+}
--- resources/zh-cn.js
+++ resources/zh-cn.js
@@ -1,36 +1,82 @@
-document.addEventListener('DOMContentLoaded', function() {
+!function(){
+
+  function updateNormativeDecl() {
     var is = document.querySelectorAll('i');
     is.forEach(function(i) {
-        if (i.innerHTML === 'This section is non-normative.') {
-            i.innerHTML = '本节是非规范的。';
-        }
+      if (i.innerHTML === 'This section is non-normative.') {
+        i.innerHTML = '本节是非规范的。';
+      }
     });
+  }
 
+  function updateTitle() {
     var update = document.querySelector('#living-standard');
     var titleEl = update.childNodes[0];
     titleEl.textContent = 'Living Standard - 更新于';
     var dateEl = update.querySelector('.pubdate');
     var date_zh = {
-        'January': 1,
-        'February': 2,
-        'March': 3,
-        'April': 4,
-        'May': 5,
-        'June': 6,
-        'July': 7,
-        'August': 8,
-        'September': 9,
-        'October': 10,
-        'November': 11,
-        'December': 12
+      'January': 1,
+      'February': 2,
+      'March': 3,
+      'April': 4,
+      'May': 5,
+      'June': 6,
+      'July': 7,
+      'August': 8,
+      'September': 9,
+      'October': 10,
+      'November': 11,
+      'December': 12
     };
     var date = dateEl.innerHTML.trim().split(/\s+/).reverse();
     dateEl.innerHTML = date[0] + '年' + date_zh[date[1]] + '月' + date[2] + '日';
+  }
 
+  function updateTOC() {
     var as = document.querySelectorAll('nav a');
     as.forEach(function(a){
         if(a.innerHTML.trim() === 'Table of Contents'){
             a.innerHTML = '目录';
         }
     });
-});
+  }
+
+  function addTranslateInfo() {
+    document.querySelectorAll('.translate-info').forEach(function(popup) {
+      var header = popup.previousElementSibling;
+      header.appendChild(popup);
+
+      var enHref = 'https://html.spec.whatwg.org' + location.pathname.replace(/^\/html/, '') + location.search + location.hash;
+      popup.innerHTML =
+        '<p class="guide">' + 
+        '  <a target="_blank" href="' + popup.dataset.zhFile + '" class="btn-link-1">我来翻译</a>' +
+        '  <a target="_blank" href="https://github.com/whatwg-cn/html/wiki/翻译指南" class="btn-link-2">翻译指南</a>' +
+        '  <a target="_blank" href="' + popup.dataset.enFile + '" class="btn-link-3">英文源码</a>' +
+        '</p>' + 
+        '<hr/>' +
+        '<p class="meta">' + 
+        '  <a href="' + enHref + '" class="btn-link">查看原文</a>' + 
+        '  <span class="date">' + popup.dataset.enDate + '</span>' +
+        '</p>' +
+        '<p class="meta">' + 
+        '  <span>中文翻译</span>' + 
+        '  <span class="date">' + (popup.dataset.zhDate || '尚未翻译') + '</span>' +
+        '</p>';
+    });
+
+    function createLink(href, content, title) {
+      var a = document.createElement('a');
+      a.href = href;
+      a.innerText = content;
+      a.title = title;
+      return a;
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    updateNormativeDecl();
+    updateTitle();
+    updateTOC();
+    addTranslateInfo();
+  });
+}();
