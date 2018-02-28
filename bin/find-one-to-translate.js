65a370037f573c5b6d03578538f582a25aa16a9b
harttle <harttle@harttle.com>
2018-03-01 01:50:50 +0800

    remove native: use gitlog insteadof nodegit

--- bin/find-one-to-translate.js
+++ bin/find-one-to-translate.js
@@ -11,19 +11,19 @@ let files = {};
 
 markFiles(path.resolve(__dirname, '../src'))
 
-dateForFiles()
-  .then(dates => Object.keys(dates).reverse().forEach(file => {
-    let match = /^(.*)\.en\.html$/.exec(file);
-    if (match) {
-      let zhFile = path.resolve(__dirname, '..', match[1] + '.zh.html');
-      if (!files[zhFile]) {
-        let date = dates[file];
-        let mm = moment(date);
-        console.log(file, mm.fromNow());
-      }
+let dates = dateForFiles()
+
+Object.keys(dates).reverse().forEach(file => {
+  let match = /^(.*)\.en\.html$/.exec(file);
+  if (match) {
+    let zhFile = path.resolve(__dirname, '..', match[1] + '.zh.html');
+    if (!files[zhFile]) {
+      let date = dates[file];
+      let mm = moment(date);
+      console.log(file, mm.fromNow());
     }
-  }))
-  .catch(e => console.error(e));
+  }
+})
 
 function markFiles(dir) {
   fs.readdirSync(dir).forEach(file => {
--- bin/merge.js
+++ bin/merge.js
@@ -21,62 +21,60 @@ process.argv.forEach(arg => {
   }
 });
 
-dateForFiles().then(dates => {
-  while (match = rFile.exec(skeleton)) {
-    let enPath = 'src/' + match[1] + '.en.html';
-    let zhPath = 'src/' + match[1] + '.zh.html';
+let dates = dateForFiles();
+while (match = rFile.exec(skeleton)) {
+  let enPath = 'src/' + match[1] + '.en.html';
+  let zhPath = 'src/' + match[1] + '.zh.html';
 
-    let enModified = modified(enPath);
-    let zhModified = modified(zhPath);
-    let content;
-    try {
-      content = fs.readFileSync(path.resolve(root, zhPath), 'utf8');
-    } catch (e) {
-        if (e.code === 'ENOENT') {
-          content = fs.readFileSync(path.resolve(root, enPath), 'utf8');
-        } else {
-          throw e;
-        }
-    }
-
-    let zhLink = `https://github.com/whatwg-cn/html/edit/master/${zhPath}`;
-    let enLink = `https://github.com/whatwg-cn/html/blob/master/${enPath}`;
-
-    if (!zhModified) {
-      let content = '从 ' + enLink + ' 拷贝英文源码至此，开始翻译';
-      zhLink = `https://github.com/whatwg-cn/html/new/master/${zhPath}`;
-      zhLink += '?filename=' + path.basename(zhPath);
-      zhLink += '&value=' + encodeURIComponent(content);
-    }
-
-    content = content.replace(
-      /<h[2-6](\s[^>]+)?>[\s\S]*?<\/h[2-6]>/,
-      (match) => {
-        let attr =  'class="translate-info"';
-        attr += ` data-zh-file="${zhLink}"`;
-        attr += ` data-zh-date="${zhModified}"`;
-        attr += ` data-en-file="${enLink}"`;
-        attr += ` data-en-date="${enModified}"`;
-        return `${match}\n<div ${attr}></div>`;
+  let enModified = modified(enPath);
+  let zhModified = modified(zhPath);
+  let content;
+  try {
+    content = fs.readFileSync(path.resolve(root, zhPath), 'utf8');
+  } catch (e) {
+      if (e.code === 'ENOENT') {
+        content = fs.readFileSync(path.resolve(root, enPath), 'utf8');
+      } else {
+        throw e;
       }
-    )
-
-    target += skeleton.slice(begin, match.index);
-    target += content;
-    begin = match.index + match[0].length;
   }
 
-  target += skeleton.slice(begin);
-  process.stdout.write(target);
+  let zhLink = `https://github.com/whatwg-cn/html/edit/master/${zhPath}`;
+  let enLink = `https://github.com/whatwg-cn/html/blob/master/${enPath}`;
 
-  function modified(file) {
-    let date = dates[file];
-    if (!date) {
-      return '';
+  if (!zhModified) {
+    let content = '从 ' + enLink + ' 拷贝英文源码至此，开始翻译';
+    zhLink = `https://github.com/whatwg-cn/html/new/master/${zhPath}`;
+    zhLink += '?filename=' + path.basename(zhPath);
+    zhLink += '&value=' + encodeURIComponent(content);
+  }
+
+  content = content.replace(
+    /<h[2-6](\s[^>]+)?>[\s\S]*?<\/h[2-6]>/,
+    (match) => {
+      let attr =  'class="translate-info"';
+      attr += ` data-zh-file="${zhLink}"`;
+      attr += ` data-zh-date="${zhModified}"`;
+      attr += ` data-en-file="${enLink}"`;
+      attr += ` data-en-date="${enModified}"`;
+      return `${match}\n<div ${attr}></div>`;
     }
+  )
 
-    let mm = moment(date);
-    return mm.format() + '（' + mm.fromNow() + '）'
+  target += skeleton.slice(begin, match.index);
+  target += content;
+  begin = match.index + match[0].length;
+}
+
+target += skeleton.slice(begin);
+process.stdout.write(target);
+
+function modified(file) {
+  let date = dates[file];
+  if (!date) {
+    return '';
   }
-})
-.catch(e => console.error(e));
+
+  let mm = moment(date);
+  return mm.format() + '（' + mm.fromNow() + '）'
+}
